9f7b8a972ff3363db88f582518513b936ea41e83
kimey <kimey_16@hotmail.com>
2021-09-09 17:02:37 -0300

    PHP corregido

--- programa1.php
+++ programa1.php
@@ -1,22 +1,19 @@
 <?php
+
 $M1 = $_GET['M1'];
 $M2 = $_GET['M2'];
 $M3 = $_GET['M3'];
 $M4 = $_GET['M4'];
 
-$N1 = 1;
-$N2 = 0.5;
-$N3 = 0.25;
-$N4 = 0.10;
+($N1 = 1);
+($N2 = 0.5);
+($N3 = 0.25);
+($N4 = 0.10);
 
-$PESO1 = $M1 * $N1;
-$PESO2 = $M2 * $N2;
-$PESO3 = $M3 * $N3;
-$PESO4 = $M4 * $N4;
-$SUMA = $PESO1 + $PESO2 + $PESO3 + $PESO4;
+$SUMA = ($M1*$N1) + ($M2*$N2) + ($M3*$N3) + ($M4*$N4);
 
-?><!DOCTYPE html>
-<<!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
@@ -25,6 +22,8 @@ $SUMA = $PESO1 + $PESO2 + $PESO3 + $PESO4;
 </head>
 <body>
 	<h1>Resultado</h1>
+
+
 	<?php echo "La suma de las monedas da como resultado" $SUMA; ?>
 </body>
 </html>  
